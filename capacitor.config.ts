dda4ee915d6f6c179366d81dd1363d32707ebcd3
Giuseppe Mennea <giuseppe.mennea@eng.it>
2025-05-20 17:46:29 +0200

    added appflow sdk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
   webDir: 'www',
- };
+  plugins: {
+    LiveUpdates: {
+      appId: '8ffd44db',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  } 
+};
 
 export default config;
